e961cbae1bc8196b0a0e0e752cb651ca68648222
mitsuo0114 <oono0114@gmail.com>
2019-01-08 11:48:26 +0900

    Create Simple Editor with key bindings

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React, {Component} from 'react';
 import './App.css';
 import MyEditor from './MyEditor'
+import {convertToRaw} from "draft-js";
 
 class App extends Component {
+
+    saveState = () => {
+        console.log("save state was called")
+        // localStorage.setItem('mydata', JSON.stringify(convertToRaw(editorState.getCurrentContent())));
+    };
+
     render() {
+        const texts = [
+            "1. Virtual Machine Concepts\n",
+            "There are currently a number of viewpoints suggesting what a virtual machine is, how it ought to be constructed, and what hardware and operating system implications result [1, 6, 7, 9, 12].",
+            "This papar examines computer architectures of third-generation-like machines and demonstrates a simple condition which may be tested to determine whether an architecture can support a virtual machine. \n",
+            "This condition may also be employed in machine design. \n",
+            "In the following, we specify intuitively what is meant by the above, then develop a more exact model of third-generation-like machines, and finally state and prove a sufficient condition for such a system to be virtualizable. \n"
+        ];
+
         return (
-            <MyEditor/>
+            texts.map((t, index) => {
+                return <div key={index} className="Container">
+                    <div>{index + 1}</div>
+                    <MyEditor className="MyEditor" text={t} lang_type="en" editable={false}/>
+                    <MyEditor className="MyEditor" text={t} lang_type="jp" editable={true} onSave={this.saveState}/>
+                </div>
+            })
+
         );
     }
 }
--- src/MyEditor.js
+++ src/MyEditor.js
@@ -1,17 +1,90 @@
 import React from 'react';
-import {Editor, EditorState} from 'draft-js';
+import {Editor, EditorState, ContentState, getDefaultKeyBinding, convertToRaw, KeyBindingUtil} from 'draft-js';
+import './MyEditor.css'
+
+const {hasCommandModifier} = KeyBindingUtil;
 
 class MyEditor extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {editorState: EditorState.createEmpty()};
+        this.lang_type = props.lang_type;
+        this.state = {
+            editorState: EditorState.createEmpty(),
+            text: props.text
+        };
         this.onChange = (editorState) => this.setState({editorState});
+        this.handleKeyCommand = this.handleKeyCommand.bind(this);
+        this.onSave = props.onSave
+    }
+
+    keyBindingFn(e) {
+
+        if (hasCommandModifier(e)) {
+            if (e.keyCode === 83 /* `S` key */) {
+                if (e.nativeEvent.shiftKey) {
+                    console.log("Ctrl+Shift+S pressed");
+                } else {
+                    return 'save';
+                }
+            } else if (e.keyCode === 90 /* 'Z' */) {
+                if (e.nativeEvent.shiftKey) {
+                    return "redo"
+                } else {
+                    return "undo"
+                }
+            }
+
+        }
+        return getDefaultKeyBinding(e);
+    }
+
+    handleKeyCommand(command, editorState) {
+        if (editorState.currentContent) {
+            console.log(editorState.currentContent.getPlainText());
+        }
+        if (command === 'save') {
+            this.onSave();
+            return 'handled';
+        } else if (command === "undo") {
+            this.setState({editorState: EditorState.undo(editorState)});
+            return 'handled';
+        } else if (command === "redo") {
+            this.setState({editorState: EditorState.redo(editorState)});
+            return 'handled';
+        }
+        return 'not-handled';
+    }
+
+    componentDidMount() {
+        console.log("[MyEditor.js] componentDidMount was called");
+        const content = ContentState.createFromText(this.state.text);
+        const editor = EditorState.createWithContent(content);
+        this.setState({editorState: editor})
     }
 
     render() {
-        return (
-            <Editor editorState={this.state.editorState} onChange={this.onChange}/>
-        );
+        if (this.lang_type === "jp") {
+            return (
+                <div className="jp_content">
+                    <Editor
+                        editorState={this.state.editorState}
+                        onChange={this.onChange}
+                        handleKeyCommand={this.handleKeyCommand}
+                        keyBindingFn={this.keyBindingFn}
+                    />
+
+                </div>
+            );
+        } else {
+            return (
+                <div className="en_content">
+                    <Editor
+                        editorState={this.state.editorState}
+                        onChange={this.onChange}
+                        readOnly={true}/>
+                </div>
+            );
+        }
     }
 }
 
